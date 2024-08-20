5dac342ed3f6359e18f7d81de9805c2ec1dae9d7
tactical-fluke <tacticalfluke304@gmail.com>
2024-08-20 17:34:15 +0800

    initial commit

---
+++ bet/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Wager(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=2000)
+    pot = models.IntegerField()
+
+class WagerOption(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=2000)
+    wager = models.ForeignKey(Wager, on_delete=models.CASCADE)
+
+class Bet(models.Model):
+    option = models.ForeignKey(WagerOption, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    value = models.IntegerField()
