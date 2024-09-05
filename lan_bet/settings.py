6d80163cf99c6f62900c19199e91c31ff68cc4b8
tactical-fluke <tacticalfluke304@gmail.com>
2024-09-05 16:57:58 +0800

    oh my god github actions

--- lan_bet/settings.py
+++ lan_bet/settings.py
@@ -25,6 +25,7 @@ def read_all_from_file(filename: str) -> str:
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+print(os.environ['TEST'])
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
